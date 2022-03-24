0214e43a11def1bb9194b5c4fe963c46115b2319
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-03-24 16:17:10 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+let items, nbSlide, suivant, precedent, count;
+items = document.querySelectorAll('img');
+nbSlide = items.length;
+suivant = document.querySelector('.right');
+precedent = document.querySelector('.left');
+count = 0;
+
+let app =({
+    slideSuivante () {
+        items[count].classList.remove('active');
+    
+        if(count < nbSlide - 1){
+            count++;
+        } else {
+            count = 0;
+        }
+    
+        items[count].classList.add('active')
+        console.log(count);
+        
+    },
+    slidePrecedente () {
+        items[count].classList.remove('active');
+    
+        if(count > 0){
+            count--;
+        } else {
+            count = nbSlide - 1;
+        }
+    
+        items[count].classList.add('active')
+        
+    },
+    keyPress (e) {
+        console.log(e);
+        
+        if(e.keyCode === 37){
+            app.slidePrecedente();
+        } else if(e.keyCode === 39){
+            app.slideSuivante();
+        }
+    },
+})
+
+suivant.addEventListener('click', app.slideSuivante);
+
+
+precedent.addEventListener('click', app.slidePrecedente);
+
+
+document.addEventListener('keydown', app.keyPress);
